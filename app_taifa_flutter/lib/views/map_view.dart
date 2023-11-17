639e22e9aa1c29f2a55678308efec8fa5d856a84
Aakash1417 <durairaj@ualberta.ca>
2023-11-16 23:42:51 -0700

    added pin categories

--- app_taifa_flutter/lib/views/map_view.dart
+++ app_taifa_flutter/lib/views/map_view.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapsPage extends StatefulWidget {
   @override
@@ -11,11 +12,15 @@ class _MapsPageState extends State<MapsPage> {
   Set<Marker> _markers = {};
   LatLng? _temporaryPinLocation;
   TextEditingController _pinNameController = TextEditingController();
+  Color _selectedColor = Colors.blue;
+  String? _selectedCategory;
+  List<Category> _categories = [];
 
   @override
   void initState() {
     super.initState();
     _loadPinsFromFirestore();
+    _loadCategories();
   }
 
   @override
@@ -23,9 +28,21 @@ class _MapsPageState extends State<MapsPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Map View'),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: _handleMenuSelection,
+            itemBuilder: (BuildContext context) {
+              return {'Add Category'}.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          ),
+        ],
       ),
       body: GoogleMap(
-        onMapCreated: _onMapCreated,
         onTap: _onMapTap,
         initialCameraPosition: const CameraPosition(
           target: LatLng(53.492412, -113.496737), // Initial map position
@@ -34,16 +51,21 @@ class _MapsPageState extends State<MapsPage> {
         markers: _markers,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showAddPinDialog();
-        },
+        onPressed: _temporaryPinLocation == null
+            ? null
+            : () {
+                _showAddPinDialog();
+              },
         child: Icon(Icons.add),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
     );
   }
 
-  void _onMapCreated(GoogleMapController controller) {
-    // Initialize the GoogleMapController here if needed
+  void _handleMenuSelection(String choice) {
+    if (choice == 'Add Category') {
+      _showAddCategoryDialog();
+    }
   }
 
   void _onMapTap(LatLng latLng) {
@@ -61,28 +83,129 @@ class _MapsPageState extends State<MapsPage> {
   }
 
   Future<void> _showAddPinDialog() async {
+    if (_temporaryPinLocation == null) return;
+
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           title: const Text('Add Pin'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _pinNameController,
-                decoration: const InputDecoration(labelText: 'Pin Name'),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  _addPinToFirestore();
-                  Navigator.of(context).pop(); // Close the dialog
-                },
-                child: Text('Add'),
-              ),
-            ],
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: _pinNameController,
+                  decoration: const InputDecoration(labelText: 'Pin Name'),
+                ),
+                const SizedBox(height: 16.0),
+                DropdownButton<String>(
+                  value: _selectedCategory,
+                  hint: Text(_selectedCategory ?? "Select Category"),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedCategory = newValue;
+                    });
+                  },
+                  items: _categories
+                      .map<DropdownMenuItem<String>>((Category category) {
+                    return DropdownMenuItem<String>(
+                      value: category.name,
+                      child: Row(
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundColor: category.color,
+                            radius: 10,
+                          ),
+                          SizedBox(width: 8),
+                          Text(category.name),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
           ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Add Pin'),
+              onPressed: () {
+                _addPinToFirestore();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _showAddCategoryDialog() async {
+    TextEditingController _categoryNameController = TextEditingController();
+    Color _categoryColor = Colors.blue;
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Add Category'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: _categoryNameController,
+                  decoration: const InputDecoration(labelText: 'Category Name'),
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('Pick a color!'),
+                          content: SingleChildScrollView(
+                            child: ColorPicker(
+                              pickerColor: _categoryColor,
+                              onColorChanged: (Color color) {
+                                _categoryColor = color;
+                              },
+                            ),
+                          ),
+                          actions: <Widget>[
+                            ElevatedButton(
+                              child: const Text('Got it'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: const Text('Pick Color'),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Add Category'),
+              onPressed: () {
+                FirebaseFirestore.instance.collection('categories').add({
+                  'name': _categoryNameController.text,
+                  'color': _categoryColor.value,
+                }).then((_) {
+                  // Handle successful addition
+                  Navigator.of(context).pop();
+                }).catchError((error) {
+                  // Handle errors
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         );
       },
     );
@@ -90,17 +213,22 @@ class _MapsPageState extends State<MapsPage> {
 
   void _addPinToFirestore() {
     final String pinName = _pinNameController.text.trim();
-    if (pinName.isNotEmpty && _temporaryPinLocation != null) {
+    if (pinName.isNotEmpty &&
+        _temporaryPinLocation != null &&
+        _selectedCategory != null) {
       FirebaseFirestore.instance.collection('allPins').add({
         'name': pinName,
         'latitude': _temporaryPinLocation!.latitude,
         'longitude': _temporaryPinLocation!.longitude,
+        'category': _selectedCategory,
       });
       setState(() {
         _markers.add(
           Marker(
             markerId: MarkerId(pinName),
             position: _temporaryPinLocation!,
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                _colorToHue(_selectedColor)),
             infoWindow: InfoWindow(title: pinName),
           ),
         );
@@ -110,8 +238,16 @@ class _MapsPageState extends State<MapsPage> {
     }
   }
 
+  double _colorToHue(Color color) {
+    HSLColor hsl = HSLColor.fromColor(color);
+    return hsl.hue;
+  }
+
   void _loadPinsFromFirestore() {
-    FirebaseFirestore.instance.collection('allPins').snapshots().listen((snapshot) {
+    FirebaseFirestore.instance
+        .collection('allPins')
+        .snapshots()
+        .listen((snapshot) {
       setState(() {
         _markers.clear();
         for (final doc in snapshot.docs) {
@@ -119,11 +255,15 @@ class _MapsPageState extends State<MapsPage> {
           final String name = data['name'] ?? '';
           final double latitude = data['latitude'] ?? 0.0;
           final double longitude = data['longitude'] ?? 0.0;
+          final String category = data['category'] ?? '';
+          final int colorValue = data['color'] ?? Colors.red.value;
 
           _markers.add(
             Marker(
               markerId: MarkerId(name),
               position: LatLng(latitude, longitude),
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  _colorToHue(Color(colorValue))),
               infoWindow: InfoWindow(title: name),
             ),
           );
@@ -132,9 +272,26 @@ class _MapsPageState extends State<MapsPage> {
     });
   }
 
+  void _loadCategories() {
+    // Load categories from Firestore or define some default categories
+    // Example:
+    _categories = [
+      Category(name: 'Restaurant', color: Colors.red),
+      Category(name: 'Park', color: Colors.green),
+      // Add more categories as needed
+    ];
+  }
+
   @override
   void dispose() {
     _pinNameController.dispose();
     super.dispose();
   }
+}
+
+class Category {
+  String name;
+  Color color;
+
+  Category({required this.name, required this.color});
 }
