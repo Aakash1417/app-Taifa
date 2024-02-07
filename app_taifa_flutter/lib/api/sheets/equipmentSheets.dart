9288842483cf69f8b0c140376a4956995fa25eea
Aakash1417 <durairaj@ualberta.ca>
2024-02-06 23:05:38 -0700

    Created gsheets api class, need to add credentials to .env

---
+++ app_taifa_flutter/lib/api/sheets/equipmentSheets.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class EquipmentSheetsApi {
+  static final _spreadSheetId = dotenv.env['EQUIPMENT_SHEETS_ID'];
+
+}
--- app_taifa_flutter/lib/main.dart
+++ app_taifa_flutter/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:app_taifa_flutter/views/signin.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'firebase_options.dart';
 import 'database_helper.dart';
@@ -12,6 +13,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
