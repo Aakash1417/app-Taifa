98594d371fef050fa95abe612d15c875b3b1d0d3
Aakash1417 <durairaj@ualberta.ca>
2024-03-13 23:21:56 -0600

    Ensured downloading multiple files in web and mobile versions

--- app_taifa_flutter/lib/views/upload_arcflash.dart
+++ app_taifa_flutter/lib/views/upload_arcflash.dart
@@ -1,12 +1,16 @@
-import 'dart:io';
+import 'dart:convert';
+import 'dart:io' as io;
+import 'dart:typed_data';
 
 import 'package:csv/csv.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:universal_html/html.dart' as html;
 
 class UploadDataPage extends StatefulWidget {
   @override
@@ -22,18 +26,26 @@ class _UploadDataPageState extends State<UploadDataPage> {
     );
 
     if (result != null) {
-      String? filePath = result.files.single.path;
-
-      if (filePath != null) {
-        String csvContent = await File(filePath).readAsString();
-        List<List<dynamic>> csvData =
-            const CsvToListConverter().convert(csvContent);
-        _processCSVData(csvData);
+      String csvContent = '';
+      if (kIsWeb) {
+        Uint8List? fileBytes = result.files.single.bytes;
+        if (fileBytes != null) {
+          // For web, use the bytes directly
+          csvContent = String.fromCharCodes(fileBytes);
+        }
+      } else {
+        String? filePath = result.files.single.path;
+        if (filePath != null) {
+          csvContent = await io.File(filePath).readAsString();
+        }
       }
+      List<List<dynamic>> csvData =
+          const CsvToListConverter().convert(csvContent);
+      _processCSVData(csvData);
     }
   }
 
-  void _processCSVData(List<List<dynamic>> csvData) {
+  void _processCSVData(List<List<dynamic>> csvData) async {
     for (var row in csvData) {
       // Assume the first column contains the ID
       String id = row[0].toString();
@@ -59,26 +71,32 @@ class _UploadDataPageState extends State<UploadDataPage> {
           },
         ),
       );
-      _savePdfFile(pdf, 'QR_Code_$id');
+      // works till here for web and mobile
+      await _savePdfFile(pdf, 'QR_Code_$id.pdf');
     }
   }
 
-  // for android, and ios maybe?
+// for android, and ios maybe?
   Future<void> _savePdfFile(pw.Document pdf, String fileName) async {
-    final directory = (await getExternalStorageDirectory())?.path ?? '';
-
-    // Create the Downloads folder if it doesn't exist
-    final downloadsDirectory = Directory('$directory/Downloads');
-    if (!downloadsDirectory.existsSync()) {
-      downloadsDirectory.createSync();
+    if (kIsWeb) {
+      // For web
+      Uint8List bytes = await pdf.save();
+      html.AnchorElement(
+          href:
+              "data:application/octet-stream;charset=utf-16le;base64,${base64.encode(bytes)}")
+        ..setAttribute("download", fileName)
+        ..click();
+      print('PDF saved: $fileName web');
+    } else {
+      // For mobile
+      final directory = (await getExternalStorageDirectory())?.path ?? '';
+      final filePath = '$directory/$fileName';
+      final file = io.File(filePath);
+      await file.writeAsBytes(await pdf.save(), flush: true);
+      OpenFile.open(filePath);
+      // Print the file path for debugging purposes
+      print('PDF saved: $filePath');
     }
-    final filePath = '${downloadsDirectory.path}/$fileName.pdf';
-    final file = File(filePath);
-    await file.writeAsBytes(await pdf.save(), flush: true);
-    OpenFile.open(filePath);
-
-    // Print the file path for debugging purposes
-    print('PDF saved: $filePath');
   }
 
   @override
